c27e43df428c157b12de6bdf6f6b19899dfda0c1
Nicholas Buse <busen2943@gmail.com>
2024-07-30 15:34:17 -0500

    savepoint - bar and bubble done

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ds_module_14_javascript_visualizations
 Homework for Module 14 - JavaScript Visualizations
+
+In this homework, we were given starter code in the form of a [Resource JavaScript File](https://github.com/busen1022/ds_module_14_javascript_visualizations/blob/main/Starter_Code/static/js/app.js) and [Resource HTML file](https://github.com/busen1022/ds_module_14_javascript_visualizations/blob/main/Starter_Code/index.html). We then used these as a framework to create our own Interactive Dashboard using [Customized JavaScript File](https://github.com/busen1022/ds_module_14_javascript_visualizations/blob/main/Submission/static/js/app.js) and [Customized HTML file](https://github.com/busen1022/ds_module_14_javascript_visualizations/blob/main/Submission/index.html).
+
+[![License](https://github.com/busen1022/ds_module_14_javascript_visualizations/blob/main/LICENSE)
+
+## Table of contents
+
+* [Introduction](#introduction)
+
+## Introduction
+
--- Submission/static/js/app.js
+++ Submission/static/js/app.js
@@ -25,29 +25,74 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    let sample_data = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    let info = sample_data.filter(x => x.id === sample)[0];
+    console.log(info);
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    let otu_ids = info.otu_ids;
+    let otu_labels = info.otu_labels;
+    let sample_values = info.sample_values;
 
     // Build a Bubble Chart
+    let bubble_trace = {
+      x: otu_ids,
+      y: sample_values,
+      mode: 'markers',
+      marker: {
+        color: otu_ids,
+        colorscale: 'Jet',
+        size: sample_values
+      },
+    text: otu_labels
+    };
 
+    let bubble_traces = [bubble_trace];
 
     // Render the Bubble Chart
+    let bubble_layout = {
+      title: 'Bacteria Cultures per Sample',
+      xaxis: {
+        title: 'OTU ID'
+      },
+      yaxis: {
+        title: 'Number of Bacteria'
+      }
+    };
 
+    Plotly.newPlot('bubble', bubble_traces, bubble_layout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    let bar_y = otu_ids.map(x => `OTU: ${x}`);
+    console.log(bar_y);
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
-
+    let bar_trace = {
+      x: sample_values.slice(0, 10).reverse(),
+      y: bar_y.slice(0, 10).reverse(),
+      type: 'bar',
+      marker: {
+        colorscale: 'Jet',
+        color: sample_values.slice(0, 10).reverse()
+      },
+      text: otu_labels.slice(0, 10).reverse(),
+      orientation: 'h'
+    };
 
     // Render the Bar Chart
+    let traces = [bar_trace];
 
+    // Add a title
+    let layout = {
+      title: "Top 10 Bacteria Cultures Found"
+    };
+
+  // Assign plot to "div", id "plot"
+  Plotly.newPlot("bar", traces, layout);  
+  
   });
 }
 
@@ -55,22 +100,28 @@ function buildCharts(sample) {
 function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
-    // Get the names field
+    console.log(data);
 
+    // Get the names field
+    let names = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropdown = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+    for (let i = 0; i < names.length; i++){
+      let name = names[i];
+      dropdown.append("option").text(name);
+    }
 
     // Get the first sample from the list
-
+    let default_name = names[0];
+    console.log(default_name);
 
     // Build charts and metadata panel with the first sample
-
+    buildCharts(default_name);
   });
 }
 
